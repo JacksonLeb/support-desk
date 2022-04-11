4fdf4deccf306d3641de0fac4bd6281e90795180
Jackson Leb <jacksonleb@Jacksons-MacBook-Pro.local>
2022-04-11 17:08:30 -0400

    Began adding UI to pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Header from './components/Header';
+
 
 function App() {
   return <>
@@ -17,6 +20,7 @@ function App() {
         </Routes>
       </div>
     </Router>
+    <ToastContainer />
   </>
 }
 
