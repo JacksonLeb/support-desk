6e932b1cfdb240ad9a591d174d6cb96aa3ede36d
Jackson Leb <jacksonleb@Jacksons-MacBook-Pro.local>
2022-04-13 16:03:47 -0400

    added lgoin and logout functionality

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -45,6 +45,12 @@ export const login = createAsyncThunk('auth/login', async (user, thunkAPI) => {
     }
   })
 
+//Logout User
+export const logout = createAsyncThunk('auth/logout', async () => {
+    await authService.logout()
+})
+
+
 export const authSlice = createSlice({
     name: 'auth',
     initialState,
@@ -70,6 +76,23 @@ export const authSlice = createSlice({
                 state.isLoading = false
                 state.isError = true
                 state.message = action.payload
+                state.user = null
+            })
+            .addCase(login.pending, (state) => {
+                state.isLoading = true
+            })
+            .addCase(login.fulfilled, (state, action) => {
+                state.isLoading = false
+                state.isSuccess = true
+                state.user = action.payload
+            })
+            .addCase(login.rejected, (state, action) => {
+                state.isLoading = false
+                state.isError = true
+                state.message = action.payload
+                state.user = null
+            })
+            .addCase(logout.fulfilled, (state) => {
                 state.user = null
             })
     }
